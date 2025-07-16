41a0dae730af23fa83e958ea02e21007c04b4eeb
Mirko Bozzetto <mirkouch@gmail.com>
2025-07-16 18:58:05 +0200

    chore: Update package.json and remove unused NextLead node resources
    - Added new NextLead node files: NextLeadContact, NextLeadStructure, NextLeadSale, NextLeadAction, and NextLeadList.
    - Removed legacy resource files for NextLeadAction, NextLeadContact, NextLeadList, NextLeadSale, and NextLeadStructure to clean up the project structure.

---
+++ nodes/NextLead/NextLead.node.ts
@@ -0,0 +1,5 @@
+export { NextLeadContact } from './NextLeadContact/NextLeadContact.node';
+export { NextLeadStructure } from './NextLeadStructure/NextLeadStructure.node';
+export { NextLeadSale } from './NextLeadSale/NextLeadSale.node';
+export { NextLeadAction } from './NextLeadAction/NextLeadAction.node';
+export { NextLeadList } from './NextLeadList/NextLeadList.node';
